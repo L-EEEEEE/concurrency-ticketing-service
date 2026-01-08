06ba7f9dd0d73b53349387ce5e57ecbc1cc48785
이다인 <dtp05093@gmail.com>
2026-01-08 15:41:06 +0900

    feat: CrossOrigin 설정

--- src/main/java/com/concert/ticketing/controller/TicketController.java
+++ src/main/java/com/concert/ticketing/controller/TicketController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TicketController {
 
     private final TicketingService ticketingService;
