d34b7490a063ee848becf255e33305a48979b0a7
이다인 <dtp05093@gmail.com>
2026-01-11 20:59:31 +0900

    fix: CORS 설정 테스트

--- src/main/java/com/concert/ticketing/config/WebConfig.java
+++ src/main/java/com/concert/ticketing/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl)
+                .allowedOriginPatterns("*")
+//                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
