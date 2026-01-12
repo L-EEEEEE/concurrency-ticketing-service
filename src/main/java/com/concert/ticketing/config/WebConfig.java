22a3f1d54ae58a8748ed7bb25fa82d93c5871c7a
이다인 <dtp05093@gmail.com>
2026-01-12 17:56:36 +0900

    fix: CSRF 비활성화(for test)

--- src/main/java/com/concert/ticketing/config/WebConfig.java
+++ src/main/java/com/concert/ticketing/config/WebConfig.java
@@ -14,8 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-//                .allowedOrigins(frontendUrl)
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
